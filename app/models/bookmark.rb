bee1d1bf179a7afcd50eda6882e31a3060e70269
Marc Davi <techthelaw@gmail.com>
2023-08-17 12:50:27 +0200

    push

---
+++ app/models/bookmark.rb
@@ -0,0 +1,8 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, :list_id, :comment, presence: true
+  validates :movie, uniqueness: { scope: :list }
+end
